055db999a5f592067551429facd003edb87f234b
zgl1104 <zgl1104@gmail.com>
2018-09-10 16:46:18 +0800

    练习0910

--- draw.py
+++ draw.py
@@ -1,5 +1,9 @@
+import time
 import turtle
 t = turtle.Pen()
-for x in range(360):
-    t.forward(x)
-    t.left(59)
+# for x in range(360):
+#     t.forward(x)
+#     t.left(90)
+t.pendown()
+t.forward(100)
+time.sleep(10)
