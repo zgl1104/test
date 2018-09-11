78f3af8283b04cb2c4b9d3afa1094ed0256d841f
zgl1104 <zgl1104@gmail.com>
2018-09-11 09:46:35 +0800

    显示语言修改

--- draw.py
+++ draw.py
@@ -4,6 +4,4 @@ t = turtle.Pen()
 # for x in range(360):
 #     t.forward(x)
 #     t.left(90)
-t.pendown()
 t.forward(100)
-time.sleep(10)
---
+++ look_time.py
@@ -0,0 +1,6 @@
+import time
+import os
+import sys
+
+newtime = time.time()
+print(newtime)
